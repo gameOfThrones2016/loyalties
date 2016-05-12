1f52f6f3aade28d4305173ab502e943b00ef7fe0
Oliver Stigley <email": null>
2016-05-12 20:10:43 +1200

    added new house and new character form

--- routes/index.js
+++ routes/index.js
@@ -6,33 +6,44 @@ var router = express.Router();
   res.render('houseindex', { title: 'Game of DEVs' });
 });
 
- router.get('/house/:id', function(req, res, next) {
+ //   Houses routes
+
+router.get('/house/:id', function(req, res, next) {
   res.render('houseview', { title: ' thou a Capulet?' });
 });
 
- router.get('/character/:id', function(req, res, next) {
-  res.render('characterview', { title: 'a Capulet?' });
+router.get('/newhouse', function(req, res, next) {
+  res.render('newHouse', { title: 'A New Dev' });
 });
 
+router.post('/newhouse', function(req, res, next) {
+  res.redirect('/house/:id', { title: 'A New Dev' });
+});
+
+router.get('/house/edit/:id', function(req, res, next) {
+  res.render('editHouse', { title: 'Dev of the Day' });
+});
+
+// Characters routes
 router.get('/characters/', function(req, res, next) {
   res.render('characterIndex', { title: 'Dev of the Day' });
+});
+
+router.get('/character/:id', function(req, res, next) {
+  res.render('characterview', { title: 'a Capulet?' });
 });
 
 router.get('/newcharacter', function(req, res, next) {
   res.render('newCharacter', { title: 'A New Dev' });
 });
 
-router.get('/newhouse', function(req, res, next) {
-  console.log("waaat")
-  res.render('newHouse', { title: 'A New Dev' });
+router.post('/newcharacter', function(req, res, next) {
+  res.redirect('/character/:id', { title: 'A New Dev' });
 });
 
 router.get('/character/edit/:id', function(req, res, next) {
   res.render('editCharacter', { title: 'Dev of the Day' });
 });
 
-router.get('/house/edit/:id', function(req, res, next) {
-  res.render('editHouse', { title: 'Dev of the Day' });
-});
 
 module.exports = router;
