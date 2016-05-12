bb16b1102a651b945d8a11c71f9b83464b9b9a67
Hape Cook-Porter & Oliver Stigley <pair+hapekituarangi.ostigley@devbootcamp.com>
2016-05-12 12:05:59 +1200

    started setting up sqlite3 db

---
+++ data/data.js
@@ -0,0 +1,25 @@
+{id: 1, name: "Olly", house: "Roa", houseId: "1", imagePath: "....", description: "I love podcasts, exercise, scifi, surfing and code! I'm a 1,2,3 type of guy.", created_at: Date.now(), updated_at: Date.now(), created_at: Date.now(), updated_at: Date.now()}
+{id: 2, name: "Hape", house: "Roa", houseId: "1", imagePath: "....", description: "Food, music, sports, the outdoors, health & fitness, family.", created_at: Date.now(), updated_at: Date.now(), created_at: Date.now(), updated_at: Date.now()}
+{id: 3, name: "Uili", house: "Roa", houseId: "1", imagePath: "....", description: "I like making stuff. - out of wood mainly.", created_at: Date.now(), updated_at: Date.now(), created_at: Date.now(), updated_at: Date.now()}
+{id: 4, name: "Surya", house: "Roa", houseId: "1", imagePath: "....", description: "I am a deep thinker and  I like music, dance and programming.", created_at: Date.now(), updated_at: Date.now(), created_at: Date.now(), updated_at: Date.now()}
+
+
+{id: 5, name: "Tony", house: "Hihi", houseId: "2", imagePath: "....", description: "I loved Star Trek before it was cool to love Star Trek.", created_at: Date.now(), updated_at: Date.now(), created_at: Date.now(), updated_at: Date.now()}
+{id: 6, name: "Ben", house: "Hihi", houseId: "2", imagePath: "....", description: "Activist app appropriator, passionate 'Zip' aficionado.", created_at: Date.now(), updated_at: Date.now(), created_at: Date.now(), updated_at: Date.now()}
+{id: 7, name: "Ollie", house: "Hihi", houseId: "2", imagePath: "....", description: "Hobbies: Singing duets with Vicken.", created_at: Date.now(), updated_at: Date.now(), created_at: Date.now(), updated_at: Date.now()}
+{id: 8, name: "Keanu", house: "Hihi", houseId: "2", imagePath: "....", description: "I am a deep thinker and  I like music, dance and programming.", created_at: Date.now(), updated_at: Date.now()}
+
+{id: 9, name: "Piet", house: "House EDA", houseId: "3"; imagePath: "....", description: "Piet is a great teacher, panther trainer and a former hardware nerd.  General legend and learning friend, he is loyal to House EDA.", created_at: Date.now(), updated_at: Date.now()}
+{id: 10, name: "Simon", house: "House EDA", houseId: "3"; imagePath: "....", description: "Simon is a loyal teacher and developer for House EDA.  He is incredibly reliable, carrying out high risk missions for his EDA lords.", created_at: Date.now(), updated_at: Date.now()}
+{id: 11, name: "Joshua", house: "House EDA", houseId: "3"; imagePath: "....", description: "Joshua is a puzzle solver, and one of the original founders of House EDA.  An intelligent Lord with a quick witt, he will not be out thought.", created_at: Date.now(), updated_at: Date.now()}
+{id: 12, name: "Mix", house: "House EDA", houseId: "3"; imagePath: "....", description: "Mix is a Knight of Valour, teacher and a self described open source nerd.  He serves House EDA with loyalty, and will defend his house with blood.", created_at: Date.now(), updated_at: Date.now()}
+
+{id: 13, name: "Lizzie", house: "House Kotare", houseId: "4"; imagePath: "....", description: "Lizzie embraces change, and has faith in the peolpe of the Seven Kingdoms to make their own destiny.", created_at: Date.now(), updated_at: Date.now()}
+{id: 14, name: "Rahmona", house: "House Kotare", houseId: "4"; imagePath: "....", description: "Rahmonda is originally from  Essos, across the narrow sea from Westeros. She is a dog lover, technology enthusiast, and loyal to the throne.", created_at: Date.now(), updated_at: Date.now()}
+{id: 15, name: "Jess", house: "House Kotare", houseId: "4"; imagePath: "....", description: "Jess is an artist, a coder, a linux nerd and a forest druid. She is also a worg, able to communicate with animals.", created_at: Date.now(), updated_at: Date.now()}
+{id: 16, name: "Heidi", house: "House Kotare", houseId: "4"; imagePath: "....", description: "Heidi loves change, just not today.  She is a mystic.", created_at: Date.now(), updated_at: Date.now()}
+
+{id: 1, name: "House Roa", imagePath: "#", description: "House Roa is a noble House, and one of the youngest in the realm of the Seven Kingdoms.  House Roa are the watchers of the wall, and the protectors of the realms of men.", created_at: Date.now(), updated_at: Date.now()}
+{id: 2, name: "House Hihi", imagePath: "#", description: "House Hihi is an ancient House, and are soon travelling north of the Wall, to become wildings.  Fierce intellect, and coding prowess opitmises House Hihi.", created_at: Date.now(), updated_at: Date.now()}
+{id: 3, name: "House EDA", imagePath: "#", description: "House EDA is a new house in the seven Kingdoms, but it is rapidly making a name for itself by defeating education houses.", created_at: Date.now(), updated_at: Date.now()}
+{id: 4, name: "House Kotare", imagePath: "#", description: "House Kotare is a happy house, forging it's own unique path in the Seven Kingdoms.  It's loyal members are from Westeros, as well as across the narrow seas. It is becoming one of the oldest houses in in the Seven Kingdoms.", created_at: Date.now(), updated_at: Date.now()}
